49c48e634d4b033527452b3c533c5ba3600ad14a
jakebuceac <jake.buceac@in2tel.ie>
2025-01-08 10:44:51 +0000

    Added function to write diagnostics and errors to log file

--- main.go
+++ main.go
@@ -3,8 +3,35 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "cli-todo-app/cmd"
+import (
+	"cli-todo-app/cmd"
+	"io"
+	"log"
+	"os"
+)
 
 func main() {
+	logFile, err := configureLogging()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer logFile.Close()
+
 	cmd.Execute()
+}
+
+func configureLogging() (*os.File, error) {
+	logFile, err := os.Create("logs.txt")
+
+	if err != nil {
+		return nil, err
+	}
+
+	logger := io.MultiWriter(os.Stderr, logFile)
+
+	log.SetOutput(logger)
+
+	return logFile, nil
 }
